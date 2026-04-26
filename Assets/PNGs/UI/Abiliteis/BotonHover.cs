950159345b5170319ecbbde822a117028f2150c8
Ezeee07 <168600591+Ezeee07@users.noreply.github.com>
2026-04-26 20:35:58 -0300

    UI hover

--- Assets/PNGs/UI/Abiliteis/BotonHover.cs
+++ Assets/PNGs/UI/Abiliteis/BotonHover.cs
@@ -1,17 +1,73 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class BotonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject target;
+    [SerializeField] private Transform target;
+
+    [Header("Escala")]
+    [SerializeField] private float escalaHover = 1.12f;
+    [SerializeField] private float duracion = 0.08f;
+    [SerializeField] private float bounce = 1.04f;
+
+    private Vector3 escalaOriginal;
+    private Coroutine rutinaActual;
+
+    void Awake()
+    {
+        if (target == null)
+            target = transform;
+
+        escalaOriginal = target.localScale;
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        target.SetActive(true);
+        if (rutinaActual != null)
+            StopCoroutine(rutinaActual);
+
+        rutinaActual = StartCoroutine(AnimarEscala(
+            escalaOriginal * escalaHover * bounce,
+            escalaOriginal * escalaHover
+        ));
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        target.SetActive(false);
+        if (rutinaActual != null)
+            StopCoroutine(rutinaActual);
+
+        rutinaActual = StartCoroutine(AnimarEscala(
+            escalaOriginal * 0.96f,
+            escalaOriginal
+        ));
+    }
+
+    private IEnumerator AnimarEscala(Vector3 intermedia, Vector3 final)
+    {
+        Vector3 inicio = target.localScale;
+        float t = 0f;
+
+        while (t < duracion)
+        {
+            t += Time.unscaledDeltaTime;
+            float p = t / duracion;
+            target.localScale = Vector3.Lerp(inicio, intermedia, p);
+            yield return null;
+        }
+
+        t = 0f;
+        inicio = target.localScale;
+
+        while (t < duracion)
+        {
+            t += Time.unscaledDeltaTime;
+            float p = t / duracion;
+            target.localScale = Vector3.Lerp(inicio, final, p);
+            yield return null;
+        }
+
+        target.localScale = final;
     }
 }
