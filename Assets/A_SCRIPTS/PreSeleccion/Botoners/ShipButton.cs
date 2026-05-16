7480e8fcc15a3458f8dcb322546d63778364a47b
Ezeee07 <168600591+Ezeee07@users.noreply.github.com>
2026-05-16 18:19:41 -0300

    Pre Seleccion Changes

--- Assets/A_SCRIPTS/PreSeleccion/Botoners/ShipButton.cs
+++ Assets/A_SCRIPTS/PreSeleccion/Botoners/ShipButton.cs
@@ -5,27 +5,43 @@ public class ShipButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 {
     [Header("Datos")]
     [SerializeField] private string shipName;
+
     [TextArea(2, 5)]
     [SerializeField] private string shipDescription;
+
     [SerializeField] private SO_BASESTATS shipStats;
+
     [Header("GameObjects")]
     [SerializeField] private GameObject shipMesh;
+
+    [SerializeField] private GameObject ImageShip;
+
     [Header("Selección visual")]
     [SerializeField] private GameObject selectedIndicator;
+
     [SerializeField] private AbilityController abilityController;
 
     private Coroutine hideCoroutine;
+
+    private void Awake()
+    {
+        if (ImageShip != null)
+            ImageShip.SetActive(false);
+    }
 
     public void OnClick()
     {
         PreselectionData.SetShip(shipStats, shipMesh);
         PreselectionData.SetAbility(shipStats.ability, shipMesh);
+
         ShipPreselectionManager.Instance?.OnShipSelected(this);
+
         switch (shipStats.ability)
         {
             case SelectedAbility.Cannon:
                 abilityController.CannonAveilable();
                 break;
+
             case SelectedAbility.Mortar:
                 abilityController.MortarAveilable();
                 break;
@@ -39,11 +55,18 @@ public class ShipButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
             StopCoroutine(hideCoroutine);
             hideCoroutine = null;
         }
+
+        if (ImageShip != null)
+            ImageShip.SetActive(true);
+
         SkillHoverPanel.Instance.Show(shipName, shipDescription, null);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (ImageShip != null)
+            ImageShip.SetActive(false);
+
         hideCoroutine = StartCoroutine(HideDelayed());
     }
 
@@ -51,18 +74,30 @@ public class ShipButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {
         yield return null;
         yield return null;
+
         if (EventSystem.current != null && !IsPointerOverPanel())
             SkillHoverPanel.Instance.Hide();
+
         hideCoroutine = null;
     }
 
     private bool IsPointerOverPanel()
     {
-        var pointerData = new PointerEventData(EventSystem.current) { position = Input.mousePosition };
+        var pointerData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
         var results = new System.Collections.Generic.List<RaycastResult>();
+
         EventSystem.current.RaycastAll(pointerData, results);
+
         foreach (var result in results)
-            if (result.gameObject.GetComponent<SkillHoverPanel>() != null) return true;
+        {
+            if (result.gameObject.GetComponent<SkillHoverPanel>() != null)
+                return true;
+        }
+
         return false;
     }
 
