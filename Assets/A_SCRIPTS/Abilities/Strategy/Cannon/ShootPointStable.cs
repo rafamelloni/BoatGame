e91214fb1be60c85a46faf03b88b5d651adc4312
Ezeee07 <168600591+Ezeee07@users.noreply.github.com>
2026-05-31 16:12:19 -0300

    Icons UI

--- Assets/A_SCRIPTS/Abilities/Strategy/Cannon/ShootPointStable.cs
+++ Assets/A_SCRIPTS/Abilities/Strategy/Cannon/ShootPointStable.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
 public class ShootPointStabilizer : MonoBehaviour
 {
-    [SerializeField] private Transform _boat;
-    [SerializeField] private Transform _cannon;
-    public bool inv = false;
-
+    [SerializeField] Transform _boat;
     private Vector3 _localOffsetFromBoat;
-
+    public bool inv = false;
     void Start()
     {
+        // Offset del shootpoint relativo al boat, en el espacio del boat sin tilt
         Quaternion boatYOnly = Quaternion.Euler(0f, _boat.eulerAngles.y, 0f);
         _localOffsetFromBoat = Quaternion.Inverse(boatYOnly) * (transform.position - _boat.position);
     }
-
     void LateUpdate()
     {
-        Quaternion boatYOnly = Quaternion.Euler(0f, _boat.eulerAngles.y, 0f);
-        transform.position = _boat.position + boatYOnly * _localOffsetFromBoat;
-
+        // Reconstruye la posicion usando solo la Y del boat
         if (inv)
-            transform.rotation = Quaternion.Euler(0f, _cannon.eulerAngles.y + 180f, 0f);
+        {
+            Quaternion boatYOnly = Quaternion.Euler(0f, _boat.eulerAngles.y +180f, 0f);
+            transform.position = _boat.position + boatYOnly * _localOffsetFromBoat;
+            transform.rotation = boatYOnly;
+        }
         else
-            transform.rotation = Quaternion.Euler(0f, _cannon.eulerAngles.y, 0f);
+        {
+            Quaternion boatYOnly = Quaternion.Euler(0f, _boat.eulerAngles.y, 0f);
+            transform.position = _boat.position + boatYOnly * _localOffsetFromBoat;
+            transform.rotation = boatYOnly;
+        }
+
     }
 }
