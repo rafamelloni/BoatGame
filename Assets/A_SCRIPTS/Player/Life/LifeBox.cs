5a6ccb994e965009a2c582b02e17700eb50f6ab2
Ezeee07 <168600591+Ezeee07@users.noreply.github.com>
2026-06-23 17:08:07 -0300

    Life box particles

--- Assets/A_SCRIPTS/Player/Life/LifeBox.cs
+++ Assets/A_SCRIPTS/Player/Life/LifeBox.cs
@@ -1,19 +1,35 @@
-using System.Collections;
 using UnityEngine;
 
 public class LifeBox : MonoBehaviour
 {
-    [SerializeField] ParticleSystem _particleSystem;
-    [SerializeField] float _ammount = 25f;
-    
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private ParticleSystem _lifeParticlesPrefab;
+    [SerializeField] private float _amount = 25f;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") )
+        if (other.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<RT_PlayerHealth>().Heal(_ammount);
-            if(_particleSystem != null)
+            other.GetComponent<RT_PlayerHealth>().Heal(_amount);
+
+            if (_particleSystem != null)
                 _particleSystem.Play();
-            
+
+            if (_lifeParticlesPrefab != null)
+            {
+                ParticleSystem particles = Instantiate(
+                    _lifeParticlesPrefab,
+                    other.transform.position,
+                    Quaternion.identity
+                );
+
+                particles.Play();
+
+                Destroy(
+                    particles.gameObject,
+                    particles.main.duration + particles.main.startLifetime.constantMax
+                );
+            }
 
             gameObject.SetActive(false);
         }
